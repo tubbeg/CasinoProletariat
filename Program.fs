fc01da116497c1d9f41d81a48522e56d9e219f3b
Tobias Gärdebro <tubbeg@gmail.com>
2024-09-27 01:21:01 +0200

    Extending Phaser scene.

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿open Browser
 open Fable.Core
 open Scene
+open Fable.Core.JsInterop
 
 type Card =
     | JOKER
@@ -9,12 +10,39 @@ type Card =
     | ACE
     | JACK
 
+[<Import("AUTO","phaser")>]
+let auto : int = jsNative
+
+let arcade = {|gravity={|y=200|}|}
+
+let physics =
+    createObj [
+        "default","arcade";
+        "arcade", arcade
+    ]
+
+let conf c p =
+    createObj [
+        "type", auto
+        "width", 800
+        "height", 600
+        "scene", c
+        "physics",p
+    ]
 
 
 let myScene = new Scene()
 let extScene = new SceneExtension()
 
+[<Import("Game","phaser")>]
+type Game (conf) =
+    class
+    end
 
+let g =
+    let c = conf extScene physics
+    new Game (c)
+    
 
 let div = document.createElement "div"
 div.innerHTML <- "Hello world!"
