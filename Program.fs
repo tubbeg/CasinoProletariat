6745094db10b605138d1b9c48858461cb94af299
Tobias Gärdebro <tubbeg@gmail.com>
2024-09-27 00:33:14 +0200

    First commit

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿open Browser
+open Fable.Core
+open Scene
+
+type Card =
+    | JOKER
+    | KING 
+    | QUEEN
+    | ACE
+    | JACK
+
+
+
+let myScene = new Scene()
+let extScene = new SceneExtension()
+
+
+
+let div = document.createElement "div"
+div.innerHTML <- "Hello world!"
+document.body.appendChild div |> ignore
---
+++ Scene.fs
@@ -0,0 +1,12 @@
+module Scene
+open Fable.Core
+
+
+[<Import("Scene","phaser")>]
+type Scene () =
+    class
+    end
+    
+
+type SceneExtension () =
+    inherit Scene()
