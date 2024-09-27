6501ff22d3430f42db7726b7c76659d306ff6e49
Tobias Gärdebro <tubbeg@gmail.com>
2024-09-27 19:51:53 +0200

    Added delegate to handle dragging cards

--- Bindings.fs
+++ Bindings.fs
@@ -1,16 +1,34 @@
 module Bindings
 open Fable.Core
+open System
 
 [<Import("Events.EventEmitter","phaser")>]
 type EventEmitter () =
     class
     end
 
+[<Import("InputPlugin","phaser")>]
+type Input () =
+    class
+        member this.setDraggable s b : unit = jsNative
+        member this.on event (funct ) : unit = jsNative
+    end
+
+
+[<Import("GameObject","phaser")>]
+type GameObject () =
+    class
+        member val x = 0 with get,set
+        member val y = 0 with get,set
+        member this.input : Input  = jsNative
+    end
 
 [<Import("Sprite","phaser")>]
 type Sprite () =
     class
+        inherit GameObject()
         member this.setOrigin x y  = jsNative
+        member this.setInteractive ()  = jsNative
     end
 
 [<Import("GameObjectFactory","phaser")>]
@@ -31,6 +49,8 @@ type Loader () =
         member this.image id url : unit = jsNative
     end
 
+
+
 type SceneConfig = {key:string option;active:bool option;}
 
 [<Import("Scene","phaser")>]
@@ -38,6 +58,7 @@ type Scene (config : SceneConfig) =
     class
         member this.add : ObjFactory = jsNative
         member this.load : Loader = jsNative
+        member this.input : Input = jsNative
         abstract preload : unit -> unit 
         abstract create : unit -> unit 
         abstract update : unit -> unit 
--- HandScene.fs
+++ HandScene.fs
@@ -1,7 +1,18 @@
 module HandScene
 open Bindings
+open System
 
 
+
+let dragFn ptr (gob : GameObject) x y =
+    gob.x <- x
+    gob.y <- y
+    ()
+
+//System.Func delegate
+//It's a problem with currying, normally not a problem in the F# world
+let handleDragDelegate =
+    Func<_,_,_,_,unit>( fun p g x y -> (dragFn p g x y) )
 
 type HandScene (config) =
     inherit Scene(config)
@@ -9,7 +20,9 @@ type HandScene (config) =
         this.load.image "card2" "./KINS/Clubs_Q.png"
         ()
     override this.create () =       
-        let s = this.add.sprite 10 10 "card2" 
+        let s : Sprite = (this.add.sprite 10 10 "card2").setInteractive()
+        this.input.setDraggable [|s|] true
+        this.input.on"drag" handleDragDelegate 
         () 
     override this.update () = ()
 
